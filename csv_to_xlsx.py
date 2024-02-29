82b83855b237b67f2d011689ec2da44711c44bf1
Jaclyn Cohen :) <105396093+jackieac04@users.noreply.github.com>
2024-02-29 09:23:59 -0500

    ??

--- csv_to_xlsx.py
+++ csv_to_xlsx.py
@@ -19,7 +19,7 @@ def txt_to_excel(input_txt_file, output_excel_file):
 
 
 if __name__ == "__main__":
-    input_txt_file = "/Users/jaclyncohen/Desktop/birthRecords/07282022.txt"
+    input_txt_file = "/Users/jaclyncohen/Desktop/birthRecords/newrecs/febemails.txt"
     output_excel_file = "output.xlsx"  # Desired name for the output Excel file
 
     txt_to_excel(input_txt_file, output_excel_file)
